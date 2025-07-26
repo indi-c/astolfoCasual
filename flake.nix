4be199dca2bb9fbcc4e1fd7988b35ebaf8f57743
indi-c <indigoconnelly@pm.me>
2025-07-26 22:01:55 +0100

    fixed hopefully

--- flake.nix
+++ flake.nix
@@ -1,25 +1,40 @@
 {
-  description = "GRUB theme";
+  description = "GRUB theme flake";
 
-  outputs = { self, nixpkgs }: let
-    systems = [ "x86_64-linux" "aarch64-linux" ];
-    forAllSystems = f: nixpkgs.lib.genAttrs systems (system:
-      f (import nixpkgs { inherit system; }));
-  in {
-    grubstolfo = forAllSystems (pkgs:
-      pkgs.callPackage ({ stdenv, lib }: stdenv.mkDerivation {
-        pname = "grubstolfo-casual";
-        version = "1.0";
-        src = lib.cleanSource ./.;
-        dontBuild = true;
-        installPhase = ''
-          dst="$out/share/grub/themes/mytheme"
-          mkdir -p "$dst"
-          cp -r ./* "$dst"
-        '';
-        meta.platforms = lib.platforms.linux;
-      }) {});
-    defaultPackage = forAllSystems (pkgs: self.grubstolfo.${pkgs.system});
-    packages.default = self.defaultPackage;
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05"; # or your channel
   };
+
+  outputs = { self, nixpkgs }: 
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = f:
+        builtins.listToAttrs (map (system: { name = system; value = f system; }) systems);
+    in {
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        pkgs.stdenv.mkDerivation {
+          pname = "grub-theme-mytheme";
+          version = "0.1";
+          src = pkgs.lib.cleanSource ./.;
+
+          dontBuild = true;
+          installPhase = ''
+            dst="$out/share/grub/themes/mytheme"
+            mkdir -p "$dst"
+            cp -r ./* "$dst"
+          '';
+
+          meta = {
+            description = "My GRUB theme";
+            platforms = pkgs.lib.platforms.linux;
+            license = pkgs.lib.licenses.unfreeRedistributable; # or your license
+          };
+        }
+      );
+
+      defaultPackage = forAllSystems (system: self.packages.${system});
+    };
 }
