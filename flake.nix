1377ff3e5a96ebf0914fb86754c29e611039fe06
indi-c <indigoconnelly@pm.me>
2025-07-26 21:50:32 +0100

    changed to flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "GRUB theme";
+
+  outputs = { self, nixpkgs }: let
+    systems = [ "x86_64-linux" "aarch64-linux" ];
+    forAllSystems = f: nixpkgs.lib.genAttrs systems (system:
+      f (import nixpkgs { inherit system; }));
+  in {
+    packages = forAllSystems (pkgs:
+      pkgs.callPackage ({ stdenv, lib }: stdenv.mkDerivation {
+        pname = "grubstolfo-casual";
+        version = "1.0";
+        src = lib.cleanSource ./.;
+        dontBuild = true;
+        installPhase = ''
+          dst="$out/share/grub/themes/mytheme"
+          mkdir -p "$dst"
+          cp -r ./* "$dst"
+        '';
+        meta.platforms = lib.platforms.linux;
+      }) {});
+    defaultPackage = forAllSystems (pkgs: self.packages.${pkgs.system});
+  };
+}
